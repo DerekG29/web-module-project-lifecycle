83f0e28dc3184dd2f2957eebe6988d4eaedffd00
DerekG29 <derekgrubbs29@gmail.com>
2024-04-05 20:57:49 -0400

    fetch data and update state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log('App - CDM Invoked');
+    axios.get(URL)
+      .then(res => this.setState(res.data.data))
+      .catch(err => console.error(err));
+    
+  }
+
   render() {
-    return null
+    console.log('App - Render Invoked');
+    return <div>Hello World</div>
   }
 }
